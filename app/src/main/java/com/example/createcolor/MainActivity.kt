6362de5d7dfde564ccfee3d1c77e32132bcc0ef6
wxlksy <31kiel12@gmail.com>
2025-03-01 14:12:09 +0300

    init

--- app/src/main/java/com/example/createcolor/MainActivity.kt
+++ app/src/main/java/com/example/createcolor/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.createcolor
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.SeekBar
@@ -37,18 +38,36 @@ class MainActivity : AppCompatActivity()
 
         with(dialogBinding)
         {
-            seekBarRed.setOnSeekBarChangeListener(createSeekBarListener(editTextRed, previewColorView, seekBarGreen, seekBarBlue))
-            seekBarGreen.setOnSeekBarChangeListener(createSeekBarListener(editTextGreen, previewColorView, seekBarRed, seekBarBlue))
-            seekBarBlue.setOnSeekBarChangeListener(createSeekBarListener(editTextBlue, previewColorView, seekBarRed, seekBarGreen))
+            try
+            {
+                val initialRed = editTextRed.text.toString().toIntOrNull() ?: 0
+                val initialGreen = editTextGreen.text.toString().toIntOrNull() ?: 0
+                val initialBlue = editTextBlue.text.toString().toIntOrNull() ?: 0
 
-            buttonConfirm.setOnClickListener {
-                currentColor = Color.rgb(
-                    seekBarRed.progress,
-                    seekBarGreen.progress,
-                    seekBarBlue.progress
-                )
-                binding.heartView.color = currentColor
-                dialog.dismiss()
+                seekBarRed.progress = initialRed
+                seekBarGreen.progress = initialGreen
+                seekBarBlue.progress = initialBlue
+
+                updatePreviewColor(previewColorView, seekBarRed, seekBarGreen, seekBarBlue)
+
+                seekBarRed.setOnSeekBarChangeListener(createSeekBarListener(editTextRed, previewColorView, seekBarRed, seekBarGreen, seekBarBlue))
+                seekBarGreen.setOnSeekBarChangeListener(createSeekBarListener(editTextGreen, previewColorView, seekBarRed, seekBarGreen, seekBarBlue))
+                seekBarBlue.setOnSeekBarChangeListener(createSeekBarListener(editTextBlue, previewColorView, seekBarRed, seekBarGreen, seekBarBlue))
+
+                buttonConfirm.setOnClickListener {
+                    currentColor = Color.rgb(
+                        seekBarRed.progress,
+                        seekBarGreen.progress,
+                        seekBarBlue.progress
+                    )
+                    binding.heartView.color = currentColor
+                    dialog.dismiss()
+                }
+            }
+
+            catch (e: Exception)
+            {
+                Log.e("DialogWindow", "Error", e)
             }
         }
         dialog.show()
@@ -57,18 +76,23 @@ class MainActivity : AppCompatActivity()
     private fun createSeekBarListener(
         editText: EditText,
         previewColorView: View,
-        seekBar1: SeekBar,
-        seekBar2: SeekBar
+        seekBarRed: SeekBar,
+        seekBarGreen: SeekBar,
+        seekBarBlue: SeekBar
     ) = object : SeekBar.OnSeekBarChangeListener
     {
         override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean)
         {
             editText.setText(progress.toString())
-            previewColorView.setBackgroundColor(Color.rgb(seekBar1.progress, seekBar2.progress, progress))
+            updatePreviewColor(previewColorView, seekBarRed, seekBarGreen, seekBarBlue)
         }
 
         override fun onStartTrackingTouch(seekBar: SeekBar) {}
         override fun onStopTrackingTouch(seekBar: SeekBar) {}
     }
-}
 
+    private fun updatePreviewColor(previewColorView: View, seekBarRed: SeekBar, seekBarGreen: SeekBar, seekBarBlue: SeekBar)
+    {
+        previewColorView.setBackgroundColor(Color.rgb(seekBarRed.progress, seekBarGreen.progress, seekBarBlue.progress))
+    }
+}
